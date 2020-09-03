c5cc41fcc0aadf2d1efe8340617fcb14f36c9eb2
E2028-Hamza <hrns2002@gmail.com>
2020-09-04 01:56:41 +0300

    flask2 çalışma

--- python/hands-on/flask-02-handling-routes-and-templates-on-ec2-linux2/app.py
+++ python/hands-on/flask-02-handling-routes-and-templates-on-ec2-linux2/app.py
@@ -53,36 +53,36 @@ def admin():
 #def greet(name):
 #    return f'Hello, { name }'
 
-#@app.route('/<name>')
-#def greet(name):
-#    greet_format=f"""
-#<!DOCTYPE html>
-#<html>
-#<head>
-#    <title>Greeting Page</title>
-#</head>
-#<body>
-#    <h1>Hello, { name }!</h1>
-#    <h1>Welcome to my Greeting Page</h1>
-#</body>
-#</html>
-#    """
-#    return greet_format
+@app.route('/<isim>')
+def greet(isim):
+    greet_format=f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greeting Page</title>
+</head>
+<body>
+    <h1>Hello, { isim }!</h1>
+    <h1>Welcome to my Greeting Page</h1>
+</body>
+</html>
+    """
+    return greet_format
 
 # Create a function named greet_admin which redirect the request to the hello path with parameter of 'Master Admin!!!!' 
 # and assign to the route of ('/greet-admin')
 
 @app.route('/greet-admin')
 def greet_admin():
-    return redirect(url_for('greet', name = 'Master Admin!!!!'))
+    return redirect(url_for('greet', isim = 'Master Admin Hamza!!!!'))
 
 
 # Rewrite a function named greet which which uses template file named `greet.html` under `templates` folder 
 # and assign to the dynamic route of ('/<name>')
 
 @app.route('/<name>')
-def greet(name):
-    return render_template('greet.html', isim = name)
+def greet1(name):
+    return render_template('greet.html', ad = name)
 
 
 # Create a function named list10 which creates a list counting from 1 to 100 within `list100.html` 
@@ -104,5 +104,5 @@ def evens():
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 
 if __name__ == '__main__':
-    #app.run(debug = True)
+    app.run(debug = True)
     app.run(host='0.0.0.0', port=80)
