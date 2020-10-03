1740cd30dce9038367005fb56270648b6d9e01b4
E2028-Hamza <hrns2002@gmail.com>
2020-10-03 07:21:52 +0300

    yeni dosya eklendi

--- python/coding-challenges/cc-002-convert-millisecs-to-hours-mins-secs/millsec.py
+++ python/coding-challenges/cc-002-convert-millisecs-to-hours-mins-secs/millsec.py
@@ -6,4 +6,4 @@ def convert(milliseconds):
     minutes = milliseconds_left // minutes_in_milliseconds
     milliseconds_left %= minutes_in_milliseconds
     seconds = milliseconds_left // 1000
-return f'{hours} hour/s'*(hours != 0) + f' {minutes} minute/s'*(minutes != 0) + f' {seconds} second/s' *(seconds != 0) or f'just {milliseconds} millisecond/s' * (milliseconds < 1000)
+    return f'{hours} hour/s'*(hours != 0) + f' {minutes} minute/s'*(minutes != 0) + f' {seconds} second/s' *(seconds != 0) or f'just {milliseconds} millisecond/s' * (milliseconds < 1000)
